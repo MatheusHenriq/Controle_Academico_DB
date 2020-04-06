67f61e6bdd3501a3f8cf7a5009d72e86c476c7bf
Matheus Henrique de Souza Fontenele <51083627+MatheusHenriq@users.noreply.github.com>
2020-04-06 10:24:31 -0300

    Update dcl.sql

--- dcl.sql
+++ dcl.sql
@@ -7,3 +7,5 @@ GRANT SELECT on disciplinas TO professores WITH GRANT OPTION;
 GRANT SELECT on disciplinas TO alunos WITH GRANT OPTION;
 GRANT SELECT on Matricula TO alunos WITH GRANT OPTION;
 GRANT SELECT on aluno TO alunos WITH GRANT OPTION;
+
+ALTER ROLE adm WITH SUPERUSER;
