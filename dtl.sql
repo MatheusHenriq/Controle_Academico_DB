50c0442f09156f9f218375435d955c4fe61532a4
Matheus Henrique de Souza Fontenele <51083627+MatheusHenriq@users.noreply.github.com>
2020-04-07 10:42:38 -0300

    Update dtl.sql

--- dtl.sql
+++ dtl.sql
@@ -3,3 +3,18 @@ BEGIN;
 SAVEPOINT modificando_notas;
 	UPDATE matricula SET nota2 = 7.00 WHERE id = 3;
 COMMIT;
+
+BEGIN;
+	INSERT INTO matricula (id,matricula_aluno,id_disciplina,nota1,nota2,nota3)
+	VALUES (6,1,3,9.00,10.00,8.50);
+SAVEPOINT insert_matricula;
+	INSERT INTO matricula (id,matricula_aluno,id_disciplina,nota1,nota2,nota3)
+	VALUES (7,2,5,8.00,7.00,6.50);
+	INSERT INTO matricula (id,matricula_aluno,id_disciplina,nota1,nota2,nota3)
+	VALUES (8,3,2,10.00,10.00,8.50);
+	INSERT INTO matricula (id,matricula_aluno,id_disciplina,nota1,nota2,nota3)
+	VALUES (9,4,1,7.00,10.00,6.50);
+	INSERT INTO matricula (id,matricula_aluno,id_disciplina,nota1,nota2,nota3)
+	VALUES (10,5,2,8.00,10.00,8.50);
+-- ROLLBACK TO SAVEPOINT insert_matricula;
+COMMIT;
